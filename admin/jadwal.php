94b79a0ae0ddf554ce80addc3eda53c540f62a0d
Grace Yosheva <151107209+13323021GraceYosheva@users.noreply.github.com>
2024-04-26 08:18:34 +0700

    Folder Admin dan Assets
    Mengubah yang sebelumnya html menjadi php dan mensesuaikan web kami dengan desain yang ada di figma

---
+++ admin/jadwal.php
@@ -0,0 +1,51 @@
+<?php include '../include/Components/admin/header.php' ?>
+<?php include '../include/Components/admin/navbar.php' ?>
+<?php include '../include/Components/admin/sidebar.php' ?>
+
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="continer-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Data Jadwal Dokter</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item active">Data Jadwal Dokter</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+            <div class="col-md-12">
+                    <div class="card card-success card-outline">
+                        <div class="card-header">
+                            <div class="card-title">Data Kegiatan</div>
+                            <div class="card-tools">
+                                <a href="#" class="btn btn-success btn-sm"><i class="fas fa-plus"></i>Tambah</a>
+                            </div>
+
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <th>No</th>
+                                        <th>Aksi</th>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="card-footer"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php include '../include/Components/admin/footer.php' ?>
---
+++ admin/puskesmas.php
@@ -0,0 +1,30 @@
+<?php include '../include/Components/admin/header.php' ?>
+<?php include '../include/Components/admin/navbar.php' ?>
+<?php include '../include/Components/admin/sidebar.php' ?>
+
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="continer-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Data Puskesmas</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item active">Data Puskesmas</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php include '../include/Components/admin/footer.php' ?>
