8344afdeadbb830e26004b062c1ac416d7f4ba84
Immanuel013 <161665988+Immanuel013@users.noreply.github.com>
2024-04-25 19:52:21 -0700

    Menambahkan folder db, include, dan uploads

---
+++ include/Components/admin/footer.php
@@ -0,0 +1,10 @@
+
+</div>
+<script src="../assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/dist/js/adminlte.min.js?v=3.2.0"></script>
+<script src="../assets/js/script.js"></script>
+</body>
+
+</html>
---
+++ include/Components/auth/header.php
@@ -0,0 +1,18 @@
+<?php include './include/Database/DBConnections.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/img/logo_transparent.png" type="image/x-icon">
+    <title>Puskesmas Janjimatogu</title>
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="assets/dist/css/adminlte.min.css?v=3.2.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class="hold-transition login-page">
+    
+    
